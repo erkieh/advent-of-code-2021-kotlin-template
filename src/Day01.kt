1fbcdd738f3955b50fa2aff462e0ba9df7374b32
Erki <8049910+erkieh@users.noreply.github.com>
2021-12-01 23:32:26 +0200

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,46 @@
+import java.util.*
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        if (input.size < 2)
+            return 0
+        var count = 0
+        input.map(Integer::parseInt).reduce { acc, i ->
+            if (i > acc)
+                count++
+            i
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        if (input.size < 4)
+            return 0
+        val ints = input.map(Integer::parseInt)
+        val window = LinkedList(ints.subList(0, 3))
+        var count = 0
+        var previousSum = window.sum()
+        ints.subList(3, ints.size).forEach { depth: Int ->
+            val currentSum = with(window) {
+                remove()
+                add(depth)
+                sum()
+            }
+            if (currentSum > previousSum)
+                count++
+            previousSum = currentSum
+        }
+        return count
     }
 
-    // test if implementation meets criteria from the description, like:
+// test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
+
+
     println(part1(input))
     println(part2(input))
 }
