5e2762b04ba8a580a44357822f906f8899a63884
Erki <8049910+erkieh@users.noreply.github.com>
2021-12-02 09:12:02 +0200

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,36 +1,28 @@
-import java.util.*
-
 fun main() {
     fun part1(input: List<String>): Int {
         if (input.size < 2)
             return 0
-        var count = 0
-        input.map(Integer::parseInt).reduce { acc, i ->
-            if (i > acc)
-                count++
-            i
-        }
-        return count
+        val ints = input.map(Integer::parseInt)
+        return ints.subList(1, ints.size).fold(0 to ints.first()) { acc, i ->
+            if (i > acc.second)
+                acc.first + 1 to i
+            else
+                acc.first to i
+        }.first
     }
 
     fun part2(input: List<String>): Int {
         if (input.size < 4)
             return 0
         val ints = input.map(Integer::parseInt)
-        val window = LinkedList(ints.subList(0, 3))
-        var count = 0
-        var previousSum = window.sum()
-        ints.subList(3, ints.size).forEach { depth: Int ->
-            val currentSum = with(window) {
-                remove()
-                add(depth)
-                sum()
-            }
-            if (currentSum > previousSum)
-                count++
-            previousSum = currentSum
-        }
-        return count
+
+        return (3 until ints.size).fold(0 to ints.subList(0, 3).sum()) { acc, i ->
+            val sum = ints.subList(i-2, i+1).sum()
+            if (sum > acc.second)
+                acc.first + 1 to sum
+            else
+                acc.first to sum
+        }.first
     }
 
 // test if implementation meets criteria from the description, like:
